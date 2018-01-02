3c9c703ce1bbda365a59649b80bcf10e5870a735
Judd <jmowbray@vtmgroup.com>
2018-01-02 15:34:24 -0800

    up8

---
+++ resources/home/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+.content{
+    margin: auto;
+    width: 1000px;
+    border: 1px solid #ccc;
+}
+</style>
+<div class="content">
+    
+</div>
+@endsection
