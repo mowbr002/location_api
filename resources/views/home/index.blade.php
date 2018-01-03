e1a4a65915fab488f60cd410a84c7a2d36965901
Judd <jmowbray@vtmgroup.com>
2018-01-02 20:14:22 -0800

    up8

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,15 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-<style>
-
-.content_wrap{
-    margin: auto;
-    width: 1000px;
-    border: 1px solid #ccc;
-}
-</style>
-
 <div class="content">
     <div class="content_wrap">
     &nbsp;
