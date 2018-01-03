05a69b8f5ca62d16a8511260d147bbdb7092943a
Judd <jmowbray@vtmgroup.com>
2018-01-02 20:41:21 -0800

    up8

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,26 @@
 @section('content')
 <div class="content">
     <div class="content_wrap">
-    &nbsp;
+        <div class="section_header">
+            
+        </div>
+        <div class="flex_container">
+            <div class="index_menu_component">
+                &nbsp;
+            </div>
+            <div class="index_menu_component">
+                &nbsp;
+            </div>
+            <div class="index_menu_component">
+                &nbsp;
+            </div>
+            <div class="index_menu_component">
+                &nbsp;
+            </div>
+            <div class="index_menu_component">
+                &nbsp;
+            </div>
+        </div>
     </div>
 </div>
 @endsection
